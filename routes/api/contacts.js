c55e9c44dc26025757116055945ca58c82052064
prodanserega <74560554+prodanserega@users.noreply.github.com>
2021-10-24 11:47:52 +0300

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const {
+  getContacts,
+  getContact,
+  saveContact,
+  removeContact,
+  updateContact,
+  updateStatusContact,
+} = require("../../controllers/contacts");
+const {
+  validateContact,
+  validateStatusContact,
+  validateId,
+} = require("./validation");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", getContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", validateId, getContact);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateContact, saveContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:id", validateId, removeContact);
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:id", [validateId, validateStatusContact], updateContact);
 
-module.exports = router
+router.patch(
+  "/:id/favorite/",
+  [validateId, validateStatusContact],
+  updateStatusContact
+);
+
+module.exports = router;
